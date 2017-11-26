36da9b56ae31594eb895243b0dce87d00149ea3a
AleFelix <alejandro.dunogent@gmail.com>
2017-11-26 01:21:53 -0300

    Trying to remove the references to the undecorated windows when the close function is called

--- hide-title-bar@alefelix/applet.js
+++ hide-title-bar@alefelix/applet.js
@@ -6,7 +6,7 @@ function TitleBarHider(orientation, panel_height, instance_id) {
     this._init(orientation, panel_height, instance_id);
 }
 
-function containsObject(list, obj) {
+function contains_object(list, obj) {
     for (var i = 0; i < list.length; i++) {
         if (list[i] === obj) {
             return [true, i];
@@ -15,22 +15,43 @@ function containsObject(list, obj) {
     return [false, -1];
 }
 
-function restoreAllTitleBars() {
-    for (let gdk_win of global.windows_with_hidden_title_bars) {
+function restore_all_title_bars() {
+    let winds = global.windows_with_hidden_title_bars;
+    for (var pos = 0; pos < winds.length; pos++) {
         try {
-            gdk_win.set_decorations(Gdk.WMDecoration.ALL);
+            winds[pos].set_decorations(Gdk.WMDecoration.ALL);
         } catch(error) {
             global.log("Can't restore the titlebar of the window, skipping");
         }
-        global.windows_with_hidden_title_bars.shift();
     }
+    Gdk.Window.process_all_updates();
+    global.windows_with_hidden_title_bars = [];
 }
 
 function edit_cinnamon_shutdown_sequence() {
     var old_shutdown_sequence = Main.do_shutdown_sequence;
     Main.do_shutdown_sequence = function() {
-        restoreAllTitleBars();
+        restore_all_title_bars();
         old_shutdown_sequence();
+    }
+}
+
+function remove_from_window_list(window) {
+    let [found, index] = contains_object(global.windows_with_hidden_title_bars, window);
+    if (found) {
+        global.windows_with_hidden_title_bars.splice(index, 1);
+        //TODO: Find a way to get the id of the window, to avoid storing the references in the array
+        window.unref();
+    }
+}
+
+//Not working, win.destroy() is not called when the user press the close button...
+//TODO: Find which function is called when the user close a window
+function edit_window_destroy_behavior(window) {
+    var old_destroy = window.destroy;
+    window.destroy = function() {
+        remove_from_window_list(window);
+        old_destroy();
     }
 }
 
@@ -51,24 +72,22 @@ TitleBarHider.prototype = {
         let screen = Gdk.Screen.get_default();
         let gdk_win = screen.get_active_window();
         if (gdk_win) {
-            let [is_hidden, index] = containsObject(global.windows_with_hidden_title_bars, gdk_win);
+            let [is_hidden, index] = contains_object(global.windows_with_hidden_title_bars, gdk_win);
             if (is_hidden) {
                 gdk_win.set_decorations(Gdk.WMDecoration.ALL);
-                global.windows_with_hidden_title_bars.splice(index, 1);
-                gdk_win.unref();
+                remove_from_window_list(gdk_win);
             } else {
+                edit_window_destroy_behavior(gdk_win);
                 global.windows_with_hidden_title_bars.push(gdk_win);
                 gdk_win.set_decorations(Gdk.WMDecoration.BORDER);
                 let [x, y] = gdk_win.get_position();
-                gdk_win.move(0,0);
-                gdk_win.resize(gdk_win.get_width() + x, gdk_win.get_height() + y);
-                gdk_win.unref();
+                gdk_win.moveresize(0,0,gdk_win.get_width() + x, gdk_win.get_height() + y);
             }
         }
     },
 
     on_applet_removed_from_panel: function() {
-        restoreAllTitleBars();
+        restore_all_title_bars();
     }
 };
 
